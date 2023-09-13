3ed6d23b0a63f054b210da7f5ca6f3e02b55bc5c
Peter Lombardo <peterllombardo@gmail.com>
2023-09-13 12:53:04 -0400

    Updated User model

--- models/user.js
+++ models/user.js
@@ -1,6 +1,7 @@
 /** User class for message.ly */
-
-
+const bcrypt = require("bcrypt");
+const db = require("../db");
+const ExpressError = require("./expressError");
 
 /** User of the site. */
 
@@ -10,20 +11,53 @@ class User {
    *    {username, password, first_name, last_name, phone}
    */
 
-  static async register({username, password, first_name, last_name, phone}) { }
+  static async register({username, password, first_name, last_name, phone}) {
+    const hashedPwd = bcrypt.hash(password, 12);
+    const results = await db.query(`
+    INSERT INTO users (username, password, first_name, last_name, phone)
+    VALUES ($1, $2, $3, $4, $5)
+    RETURNING username`,
+    [username, hashedPwd, first_name, last_name, phone]);
+    return results.rows[0];
+   }
 
   /** Authenticate: is this username/password valid? Returns boolean. */
 
-  static async authenticate(username, password) { }
+  static async authenticate(username, password) {
+    const result = await db.query(`
+    SELECT password
+    FROM users
+    WHERE username=$1`,
+    [username]);
+
+    return user && await bcrypt.compare(password, user.password);
+   }
 
   /** Update last_login_at for user */
 
-  static async updateLoginTimestamp(username) { }
+  static async updateLoginTimestamp(username) { 
+    const result = await db.query(`
+    UPDATE users
+    SET SET last_login_at = current_timestamp
+    WHERE username = $1
+    RETURNING username`,
+    [username]);
+    if (!result.rows[0]) {
+      throw new ExpressError(`User ${username} does not exist.`, 404);
+    }
+  }
 
   /** All: basic info on all users:
    * [{username, first_name, last_name, phone}, ...] */
 
-  static async all() { }
+  static async all() {
+    const results = await db.query(`
+    SELECT username, first_name, last_name, phone
+    FROM users
+    ORDER BY username`);
+
+    return results.rows
+   }
 
   /** Get: get user by username
    *
@@ -34,7 +68,14 @@ class User {
    *          join_at,
    *          last_login_at } */
 
-  static async get(username) { }
+  static async get(username) {
+    const result = await db.query(`
+    SELECT username, first_name, last_name, phone, join_at, last_login_at
+    FROM users
+    WHERE username=$1`,
+    [username]);
+    return result.rows[0];
+   }
 
   /** Return messages from this user.
    *
@@ -44,7 +85,33 @@ class User {
    *   {username, first_name, last_name, phone}
    */
 
-  static async messagesFrom(username) { }
+  static async messagesFrom(username) {
+    const results = await db.query(`
+    SELECT m.id,
+    m.to_username,
+    u.first_name,
+    u.last_name,
+    u.phone,
+    m.body,
+    m.sent_at,
+    m.read_at
+    FROM messages AS m
+    JOIN users AS u ON m.to_username = u.username
+    WHERE from_username = $1`,
+    [username]);
+
+    return results.rows.map(m => ({
+    id: m.id,
+    to_user: {
+              username: m.to_username,
+              first_name: m.first_name,
+              last_name: m.last_name,
+              phone: m.phone},
+          body: m.body,
+          sent_at: m.sent_at,
+          read_at: m.read_at}));
+}
+  
 
   /** Return messages to this user.
    *
@@ -54,7 +121,33 @@ class User {
    *   {username, first_name, last_name, phone}
    */
 
-  static async messagesTo(username) { }
+  static async messagesTo(username) {
+    const results = db.query(`
+    SELECT m.id,
+          m.from_username,
+          u.first_name,
+          u.last_name,
+          u.phone,
+          m.body,
+          m.sent_at,
+          m.read_at
+    FROM messages AS m
+    JOIN users AS u ON m.from_username = u.username
+    WHERE to_username=$1`,
+    [username]);
+    return results.rows.map(m => ({
+      id: m.id,
+      from_user: {
+        username: m.from_username,
+        first_name: m.first_name,
+        last_name: m.last_name,
+        phone: m.phone,
+      },
+      body: m.body,
+      sent_at: m.sent_at,
+      read_at: m.read_at
+    }));
+  }
 }
 
 
